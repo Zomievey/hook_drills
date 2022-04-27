a81975e1fd8400083ed1d6801902d9e453cb5653
Zomievey <haylee_Hunter@aol.com>
2022-04-26 23:01:16 -0500

    added useRef hook use case

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import React, { useState, useReducer, useEffect } from "react";
+import React, { useState, useReducer, useEffect, useRef } from "react";
 import axios from "axios";
 
 const reducer = (state, action) => {
@@ -49,10 +49,26 @@ function App() {
     setName(newValue);
   };
 
+  const [addName, setAddName] = useState("");
+  const [newName, setNewName] = useState("Username");
+  
+  
+  let changeName = (e) => {
+    const changeNameValue = e.target.value;
+    setAddName(changeNameValue);
+  };
+
+  const inputRef = useRef(null);
+  const handleClick = () => {
+    inputRef.current.value = "";
+    setNewName(addName);
+    //setting it equal to an empty string clears the input field
+  };
+
   return (
     <>
-      <div className="mb-3 mt-3">
-        <h5>This portion uses the useState hook:</h5>
+      <div className="mb-5 mt-3">
+        <h5>1. This portion uses the useState hook:</h5>
         {counter}{" "}
         <button
           className="btn btn-warning mx-5"
@@ -65,10 +81,10 @@ function App() {
         <input placeholder="type something..." onChange={nameInput}></input>
         <div className="my-3">{name}</div>
       </div>
-      <h5>This portion uses the useReducer hook:</h5>
-      <div>
+      <h5>2. This portion uses the useReducer hook:</h5>
+      <div className="mb-5">
         <h1>{state.newCount}</h1>
-        <button
+        <button type="submit"
           className="btn btn-secondary"
           onClick={() => {
             dispatch({ type: "INCREMENT" });
@@ -80,7 +96,7 @@ function App() {
         {state.showText ? <h1>Hello</h1> : <h1>Goodbye</h1>}
       </div>
       <div className="mb-5">
-        <h5>This portion uses the useEffect hook:</h5>
+        <h5>3. This portion uses the useEffect hook:</h5>
         <h1>Hello, {info.email}!</h1>
         <button
           className="btn btn-danger"
@@ -88,7 +104,20 @@ function App() {
             setAddEmail(addEmail + 1);
           }}
         >
-          next email
+          Next Email
+        </button>
+      </div>
+      <div className="mb-3">
+        <h5>4. This portion uses the useRef hook:</h5>
+        <h1>Hello, {newName}!</h1>
+        <input
+          type="text"
+          placeholder="Ex..."
+          onChange={changeName}
+          ref={inputRef}
+        />
+        <button onClick={handleClick} className="btn btn-primary mx-2">
+          Change Name
         </button>
       </div>
     </>
